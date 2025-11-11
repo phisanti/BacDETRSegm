d50c9259546733b81e5c731a5a611e778033e177
Cano-Muniz, Santiago <tisalon@outlook.com>
2025-11-11 15:25:12 +0100

    Add bacdetr-train package structure
    - Created bacdetr-train package directory with __init__.py
    - Added pyproject.toml with training dependencies
    - Configured dependency on bacdetr>=2.0.0
    - Set up package metadata and versioning (v2.0.0)
    - Included all training-specific libraries (pycocotools, fairscale, etc.)

---
+++ bacdetr-train/bacdetr_train/__init__.py
@@ -0,0 +1,5 @@
+# ------------------------------------------------------------------------
+# BacDETR-Train
+# Copyright (c) 2025. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
