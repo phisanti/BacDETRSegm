395dc60ed705b3acd1c87aea7fa8c26cfb8e28bf
Cano-Muniz, Santiago <tisalon@outlook.com>
2025-11-11 15:25:11 +0100

    Add bacdetr utility modules
    - Added box operations (IoU, generalized IoU, conversions)
    - Implemented distributed training utilities (init, sync)
    - Added COCO class definitions and mappings
    - Created file handling utilities
    - Enhanced SLURM support in init_distributed_mode
    - Updated imports from rfdetr to bacdetr namespace

---
+++ bacdetr/bacdetr/util/__init__.py
@@ -0,0 +1,5 @@
+# ------------------------------------------------------------------------
+# BacDETR
+# Copyright (c) 2025. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
